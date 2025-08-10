8b58bbf54f5382e461750388d98976d613a936a1
Truong Van Thanh <truong.van.thanh@sun-asterisk.com>
2025-08-10 23:42:45 +0900

    config cors v2

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,12 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins ENV.fetch("FE_DOMAIN", "http://localhost:5173")
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins ENV.fetch("FE_DOMAIN", "http://localhost:5173")
 
-    resource "*",
-      headers: :any,
-      methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
-  end
-end
+#     resource "*",
+#       headers: :any,
+#       methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
+#   end
+# end
