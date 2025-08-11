b89d3028c8e00a653b2f47923f482612bf34a7c2
Truong Van Thanh <truong.van.thanh@vareal.vn>
2025-08-11 14:25:08 +0700

    feature.get_users

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,17 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+      include AuthorizeRequest
+
+      def show
+        res(current_user)
+      end
+
+      def index
+        raise ApiErrors::UnauthorizedError if current_user.member?
+
+        res(User.member, as: :list)
+      end
+    end
+  end
+end
