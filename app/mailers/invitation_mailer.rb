3b3a65e9c8df0c03da6ef84a62ad0be57f37212f
Truong Van Thanh <truong.van.thanh@vareal.vn>
2025-08-13 16:25:49 +0700

    update link join project

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -3,7 +3,7 @@ class InvitationMailer < ApplicationMailer
 
   def invite_email(invitation)
     @invitation = invitation
-    @accept_url = "#{ENV["FE_DOMAIN"]}/invitations/accept?" \
+    @accept_url = "#{ENV["FE_DOMAIN"]}/projects/#{@invitation.project_id}/invitations/accept?" \
                   "token=#{@invitation.token}&email=#{CGI.escape(@invitation.email)}"
 
     mail(to: @invitation.email, subject: "Invitation #{@invitation.project.name}")
