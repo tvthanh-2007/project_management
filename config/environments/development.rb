73598ddcd9c44d5c6f6012fff8aa8eaf2048aab5
Truong Van Thanh <truong.van.thanh@vareal.vn>
2025-10-06 12:37:37 +0900

    test_job_solid_queue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,9 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  config.active_job.queue_adapter = :solid_queue
+  config.solid_queue.connects_to = { database: { writing: :queue } }
+
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
