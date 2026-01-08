6e19e794fbc3b45b2b91bbd3863603732f37903b
Truong Van Thanh <truong.van.thanh@vareal.vn>
2026-01-08 11:45:13 +0700

    config mailersend

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,7 +75,27 @@ Rails.application.configure do
   end
 
   # config/environments/development.rb
+  # -------> letter_opener_web
   config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+
+  # -------> mailersend
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address: ENV["MAILERSEND_SERVER"],
+  #   port: ENV["MAILERSEND_PORT"],
+  #   domain: ENV["MAILERSEND_DOMAIN"],
+  #   user_name: ENV["MAILERSEND_SMTP_USER"],
+  #   password: ENV["MAILERSEND_SMTP_PASSWORD"],
+  #   authentication: "plain",
+  #   enable_starttls_auto: true,
+  #   openssl_verify_mode:  "none"
+  # }
+
+  # config.action_mailer.default_options = {
+  #   from: ENV["MAILERSEND_MAIL_NO_REPLY"]
+  # }
+
+  # config.action_mailer.raise_delivery_errors = true
 end
